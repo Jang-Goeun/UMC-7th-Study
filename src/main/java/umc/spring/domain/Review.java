1d4c722b58c62897b97658eaab1953d5eab044b9
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2024-11-04 00:05:10 +0900

    Chapter05. JPA 기초 및 프로젝트 구조(1)

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,39 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.persistence.Id;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.MemberAgree;
+import umc.spring.domain.mapping.ReviewImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    private Float score;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<ReviewImage> ReviewImageList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
