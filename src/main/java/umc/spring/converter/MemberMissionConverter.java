805f7b5f60af152c7d884b5b72e011b1563b4c16
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2024-11-25 02:32:44 +0900

    ✨ [FEAT] 9주차 미션 내가 진행중인 미션 목록 조회 API 구현

--- src/main/java/umc/spring/converter/MemberMissionConverter.java
+++ src/main/java/umc/spring/converter/MemberMissionConverter.java
@@ -1,13 +1,18 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.domain.enums.MissionStatus;
 import umc.spring.web.dto.MemberMission.MemberMissionRequestDTO;
 import umc.spring.web.dto.MemberMission.MemberMissionResponseDTO;
+import umc.spring.web.dto.ReviewResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemberMissionConverter {
     public static MemberMissionResponseDTO.ChallengingResultDTO toChallengingResultDTO(MemberMission memberMission){
@@ -33,6 +38,32 @@ public class MemberMissionConverter {
                 .status(missionStatus)
                 .member(member)
                 .mission(mission)
+                .build();
+    }
+
+    // 내가 진행중인 미션 목록
+    public static MemberMissionResponseDTO.ChallengingMissionDTO challengingMissionDTO(MemberMission memberMission){
+        return MemberMissionResponseDTO.ChallengingMissionDTO.builder()
+                .reward(memberMission.getMission().getReward())
+                .missionSpec(memberMission.getMission().getMissionSpec())
+                .createdAt(memberMission.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public static MemberMissionResponseDTO.ChallengingMissionListDTO challengingMissionListDTO(Page<MemberMission> memberMissionList){
+
+        // 진행 중인 미션만 필터링
+        List<MemberMissionResponseDTO.ChallengingMissionDTO> challengingMissionDTOList = memberMissionList.stream()
+                .filter(memberMission -> memberMission.getStatus() == MissionStatus.CHALLENGING) // 필터 조건
+                .map(MemberMissionConverter::challengingMissionDTO).collect(Collectors.toList());
+
+        return MemberMissionResponseDTO.ChallengingMissionListDTO.builder()
+                .isLast(memberMissionList.isLast())
+                .isFirst(memberMissionList.isFirst())
+                .totalPage(memberMissionList.getTotalPages())
+                .totalElements(memberMissionList.getTotalElements())
+                .listSize(challengingMissionDTOList.size())
+                .reviewList(challengingMissionDTOList)
                 .build();
     }
 }
--- src/main/java/umc/spring/web/dto/MemberMission/MemberMissionResponseDTO.java
+++ src/main/java/umc/spring/web/dto/MemberMission/MemberMissionResponseDTO.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberMissionResponseDTO {
 
@@ -16,5 +18,29 @@ public class MemberMissionResponseDTO {
     public static class ChallengingResultDTO{
         Long memberMissionId;
         LocalDateTime createAt;
+    }
+
+    // 내가 진행중인 미션 목록
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChallengingMissionListDTO {
+        List<MemberMissionResponseDTO.ChallengingMissionDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChallengingMissionDTO {
+        Integer reward;
+        String missionSpec;
+        LocalDate createdAt;
     }
 }
